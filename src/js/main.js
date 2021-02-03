de7e1968482675de946df735de9b97b1f9faac9d
simon-svg <simonkarapetyan605@gmail.com>
2021-02-03 18:16:02 +0400

    added responsive

--- src/js/main.js
+++ src/js/main.js
@@ -59,67 +59,69 @@ let yellowBool = true;
 let blueBool = true;
 let greenBool = true;
 
-detailsItems[0].addEventListener("click", () => {
-    if (yellowBool) {
-        detailsItems[0].style.transform = "translateX(0)";
-    }
-    else {
-        detailsItems[0].style.transform = "translateX(calc(100% - 300px))";
-    }
-    yellowBool = !yellowBool;
-})
-detailsItems[0].addEventListener("mouseenter", () => {
-    if (yellowBool) {
-        detailsItems[0].style.transform = "translateX(calc(100% - 330px))";
-    }
-})
-detailsItems[0].addEventListener("mouseleave", () => {
-    if (yellowBool) {
-        detailsItems[0].style.transform = "translateX(calc(100% - 300px))";
-    }
-})
+function VideoEvent(vid1, vid1hov, vid2, vid2hov, vid3, vid3hov) {
+    detailsItems[0].addEventListener("click", () => {
+        if (yellowBool) {
+            detailsItems[0].style.transform = "translateX(0)";
+        }
+        else {
+            detailsItems[0].style.transform = "translateX(calc(100% - 300px))";
+        }
+        yellowBool = !yellowBool;
+    })
+    detailsItems[0].addEventListener("mouseenter", () => {
+        if (yellowBool) {
+            detailsItems[0].style.transform = "translateX(calc(100% - 330px))";
+        }
+    })
+    detailsItems[0].addEventListener("mouseleave", () => {
+        if (yellowBool) {
+            detailsItems[0].style.transform = "translateX(calc(100% - 300px))";
+        }
+    })
 
 
-detailsItems[1].addEventListener("click", () => {
-    if (blueBool) {
-        detailsItems[1].style.transform = "translateX(100px)";
-    }
-    else {
-        detailsItems[1].style.transform = "translateX(calc(100% - 200px))";
-    }
-    blueBool = !blueBool;
-})
-detailsItems[1].addEventListener("mouseenter", () => {
-    if (blueBool) {
-        detailsItems[1].style.transform = "translateX(calc(100% - 230px))";
-    }
-})
-detailsItems[1].addEventListener("mouseleave", () => {
-    if (blueBool) {
-        detailsItems[1].style.transform = "translateX(calc(100% - 200px))";
-    }
-})
+    detailsItems[1].addEventListener("click", () => {
+        if (blueBool) {
+            detailsItems[1].style.transform = "translateX(100px)";
+        }
+        else {
+            detailsItems[1].style.transform = "translateX(calc(100% - 200px))";
+        }
+        blueBool = !blueBool;
+    })
+    detailsItems[1].addEventListener("mouseenter", () => {
+        if (blueBool) {
+            detailsItems[1].style.transform = "translateX(calc(100% - 230px))";
+        }
+    })
+    detailsItems[1].addEventListener("mouseleave", () => {
+        if (blueBool) {
+            detailsItems[1].style.transform = "translateX(calc(100% - 200px))";
+        }
+    })
 
 
-detailsItems[2].addEventListener("click", () => {
-    if (greenBool) {
-        detailsItems[2].style.transform = "translateX(200px)";
-    }
-    else {
-        detailsItems[2].style.transform = "translateX(calc(100% - 100px))";
-    }
-    greenBool = !greenBool;
-})
-detailsItems[2].addEventListener("mouseenter", () => {
-    if (greenBool) {
-        detailsItems[2].style.transform = "translateX(calc(100% - 130px))";
-    }
-})
-detailsItems[2].addEventListener("mouseleave", () => {
-    if (greenBool) {
-        detailsItems[2].style.transform = "translateX(calc(100% - 100px))";
-    }
-})
+    detailsItems[2].addEventListener("click", () => {
+        if (greenBool) {
+            detailsItems[2].style.transform = "translateX(200px)";
+        }
+        else {
+            detailsItems[2].style.transform = "translateX(calc(100% - 100px))";
+        }
+        greenBool = !greenBool;
+    })
+    detailsItems[2].addEventListener("mouseenter", () => {
+        if (greenBool) {
+            detailsItems[2].style.transform = "translateX(calc(100% - 130px))";
+        }
+    })
+    detailsItems[2].addEventListener("mouseleave", () => {
+        if (greenBool) {
+            detailsItems[2].style.transform = "translateX(calc(100% - 100px))";
+        }
+    })
+}
 
 
 
@@ -131,25 +133,36 @@ const app = document.querySelector(".app");
 const appTransform = document.querySelector(".app__transform");
 const footer = document.querySelector(".footer");
 let translateZ = 0;
-let footerTransform = 0;// 590
+let footerTransform = 0;
 
 app.onwheel = (e) => {
     translateZ -= event.deltaY * 0.025;
-    footerTransform -= event.deltaY * 0.3655;
-    console.log(footerTransform)
+    footerTransform -= event.deltaY * 0.063;
 
-    if(translateZ < -40){
+    if (translateZ < -40) {
         translateZ = -40;
     }
-    else if(translateZ > 0){
+    else if (translateZ > 0) {
         translateZ = 0;
     }
-    if(footerTransform < -590){
-        footerTransform = -590;
+    if (footerTransform < -100) {
+        footerTransform = -100;
     }
-    else if(footerTransform > 0){
+    else if (footerTransform > 0) {
         footerTransform = 0;
     }
     appTransform.style.transform = `perspective(400px) translateZ(${translateZ}px)`;
-    footer.style.transform = `translateY(${footerTransform}px)`;
+    footer.style.transform = `translateY(${footerTransform}%)`;
 }
+
+
+
+
+
+
+
+
+
+window.addEventListener("resize", (e) => {
+    console.log(window.innerWidth)
+})
